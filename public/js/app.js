b843f7b04bc3f9a834eebc157345ed9b048e444b
wangmaozhu <wangmaozhu@goodsogood.com>
2019-04-15 16:04:54 +0800

    update

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,13 @@ function range(a, b) {
     return Math.floor(Math.random() * (b - a) + a);
 }
 
+function isIE() {
+    return !!window.ActiveXObject || "ActiveXObject" in window;
+}
+
+var isIEBrowser = isIE();
+
+
 (function () {
     var _window = $(window);
     var _body = $(document.body);
@@ -48,11 +55,63 @@ function range(a, b) {
     }).trigger("scroll");
 
     if($.fn.swiper){
+
+        $('.epiboly-list span').each(function () {
+            var _this = $(this);
+            var textArray = _this.html().split('');
+            _this.html('');
+            $.each(textArray, function (index, t) {
+                _this.append('<i class="p-3d">' + t + '</i>');
+            })
+        });
+
         var mySwiper = $('.epiboly-sc').swiper({
+            speed: 666,
             loop: true,
             pagination : '.pagination',
             paginationClickable :true,
+            grabCursor: true,
+            progress: !isIEBrowser,
+            // onSlideChangeStart: slideChangeStart,
+            onProgressChange: function (swiper) {
+                for (var i = 0; i < swiper.slides.length; i++) {
+                    var slide = swiper.slides[i];
+                    var progress = slide.progress;
+                    var translate = progress * swiper.width;
+                    var characters = $(slide).find('.p-3d');
+                    // characters = $.makeArray(characters).sort(function (a, b) {
+                    //     return Math.random() > 0.5 ? 1 : -1;
+                    // });
+                    for (var j = 0; j < characters.length; j++) {
+                        var charScale = progress * 1500 * (characters.length - j) / characters.length;
+                        var charOpacity = 1 - Math.min(Math.abs(progress), 1);
+                        swiper.setTransform(characters[j], 'translate3d(0,0,' + (charScale) + 'px)');
+                        characters[j].style.opacity = charOpacity;
+                    }
+                    swiper.setTransform(slide, 'translate3d(' + translate + 'px,0,0)');
+                }
+            },
+            onTouchStart: function (swiper) {
+                for (var i = 0; i < swiper.slides.length; i++) {
+                    swiper.setTransition(swiper.slides[i], 0);
+                    var characters = $(swiper.slides[i]).find('.p-3d');
+                    for (var j = 0; j < characters.length; j++) {
+                        swiper.setTransition(characters[j], 0);
+                    }
+                }
+            },
+            onSetWrapperTransition: function (swiper, speed) {
+                for (var i = 0; i < swiper.slides.length; i++) {
+                    swiper.setTransition(swiper.slides[i], speed);
+                    var characters = $(swiper.slides[i]).find('.p-3d');
+                    for (var j = 0; j < characters.length; j++) {
+                        swiper.setTransition(characters[j], speed);
+                    }
+                }
+
+            }
         });
+
 
         $('.epiboly-prev').on('click', function () {
             mySwiper.swipePrev();
